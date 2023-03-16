954b3ab495ce5093d1ba6eff0615c63a51eb95f1
N4YST <shaun05270439@gmail.com>
2023-03-16 11:19:45 +0800

    first commit

---
+++ src/main/java/com/nayst/Main.java
@@ -0,0 +1,16 @@
+package com.nayst;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.nayst
+ * Description:
+ *
+ * @Author: Nayst
+ * @Create: ${DATE} ${TIME}
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
